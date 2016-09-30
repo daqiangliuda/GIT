a35028c719f512ededd9bbd0a92555e7b10679af
liuda <wyyuelh@163.com>
2016-09-30 14:40:37 +0800

    22222222222

--- app/src/main/java/com/ylh/administrator/git/MainActivity.java
+++ app/src/main/java/com/ylh/administrator/git/MainActivity.java
@@ -10,6 +10,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        initDatas();
+    }
+
+    private void initDatas() {
     }
 
     private void init() {
